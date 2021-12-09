6beb500dc1956f1221377431bbf75456566bb57a
hamda hassan <hamda6464@gmail.com>
2021-12-09 14:46:01 +0300

    changing view

---
+++ resources/views/main.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Perfect Wheels</title>
+<link rel="stylesheet" type="text/css" href="public/css/style.css">
+</head>
+<body>
+<header>
+<div class="main">
+<div class="logo"></div>
+<img src="public/imgs/logo4.png">
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="Brands.html">Brands</a></li>
+<li><a href="About.html">About</a></li>
+<li><a href="Contact.html">Contact</a></li>
+</ul>
+</div>
+<div class="title">
+<h1 button onclick="alert('Welcome To PERFECT WHEELS Offical Website!')">PERFECT WHEELS<button></h1>
+<element id="content"></element>
+</div>
+</header>
+</ul>
+</body>
+</html>
