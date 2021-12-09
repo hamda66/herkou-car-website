d2e294a6d3f2d69717b031f74c8faf5566439997
hamda hassan <hamda6464@gmail.com>
2021-12-10 01:33:42 +0300

    adding css for all

---
+++ resources/views/about.blade.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Perfect Wheels</title>
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('/css/About.css') }}">
+</head>
+<body>
+<header>
+<div class="main">
+<div class="logo"></div>
+<img src="logo4.png">
+<ul>
+<li><a href="Home.html">Home</a></li>
+<li><a href="Brands.html">Brands</a></li>
+<li><a href="About.html">About</a></li>
+<li><a href="Contact.html">Contact</a></li>
+</ul>
+</div>
+<div class="title">
+<h1>About Us<button></h1>
+<h2>Welcome to Perfect Wheels,This Website was developed in 2021, here in Perfect Wheels we aim to trade and sell the most Well-known High-Class luxuriant Car brands in the world. The customer can look through our catelouge which contains a range of brands each brand having its own selection of cars, when the customer finds the desired car they can buy it with extremely exceptional Prices as well as great Deals.</h2>
+<element id="content"></element>
+</div>
+</header>
+</body>
+</ul>
+</html>
---
+++ resources/views/brands.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Perfect Wheels</title>
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('/css/Brand.css') }}">
+</head>
+<body>
+<header>
+<div class="main">
+<div class="logo"></div>
+<img src="logo4.png">
+<ul>
+<li><a href="Home.html">Home</a></li>
+<li><a href="Brands.html">Brands</a></li>
+<li><a href="About.html">About</a></li>
+<li><a href="Contact.html">Contact</a></li>
+</ul>
+</div>
+<div class="title">
+<h1>Brands<button></h1>
+<img class="img-BMW" src="BMW LOGO.png" >
+<img class="img-FERRARI" src="FERRARI LOGO.png" >
+<img class="img-BUGGATI" src="BUGGATI LOGO.png" >
+<img class="img-LAMBO" src="LAMBO LOGO.png" >
+<img class="img-MERCEDES" src="MERCEDES LOGO.png" >
+<img class="img-BENTLEY" src="BENTLEY LOGO.png" >
+
+<element id="content"></element>
+</div>
+</header>
+
+</ul>
+</html>
